7f9b632bceeeba88144cc69c9d49b2dbfeccc941
Tomas Hesse <tomas.hesse@consid.se>
2018-02-26 11:18:35 +0100

    Tagit bort måsvingar på anonyma funktioner

--- main.js
+++ main.js
@@ -6,11 +6,11 @@ const puppeteer = require('puppeteer');
   const page = await browser.newPage();
 //   page.once('networkidle2', () => {console.log('Page networkidle2!');});
   page.once('networkidle0', () => {console.log('Page networkidle0!');});
-   page.once('load', () =>{ console.log('Page load!');});
+   page.once('load', () => console.log('Page load!'));
   
   await page.goto('http://www.aftonbladet.se/', {waitUntil: ['networkidle2'] }).then((response) =>{console.log('then ');}).catch((reason) =>{ console.error('FELET ', reason)});
     console.log('scrrein');
-  await page.screenshot({path: 'aftonabladet.png'}).then(()=>{console.log('scriin');});
+  await page.screenshot({path: 'aftonabladet.png'}).then(()=> console.log('scriin'));
 
   await browser.close();
 })();
