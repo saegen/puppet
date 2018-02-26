9e326f84747482fa3554d9810146e3197a253d26
Tomas Hesse <tomas.hesse@consid.se>
2018-02-26 11:13:44 +0100

    Denna fungerar inte! Load sker, dvs det skrivs ut i page.once('load..  men den hänger i goto så den hamnar i catchen iställlet. Fungerar om man byter waitUntil networkidle

--- main.js
+++ main.js
@@ -6,12 +6,10 @@ const puppeteer = require('puppeteer');
   const page = await browser.newPage();
 //   page.once('networkidle2', () => {console.log('Page networkidle2!');});
   page.once('networkidle0', () => {console.log('Page networkidle0!');});
-//   page.once('load', () =>{ console.log('Page load!');});
+   page.once('load', () =>{ console.log('Page load!');});
   
-  await page.goto('http://www.aftonbladet.se/', {waitUntil: ['networkidle0'] }).then((response) =>{console.log('then ');}).catch((reason) =>{ console.error('FELET ', reason)});
-  //await page.goto('http://www.aftonbladet.se/');
-console.log('goto');
-  console.log('scrrein');
+  await page.goto('http://www.aftonbladet.se/', {waitUntil: ['load'] }).then((response) =>{console.log('then ');}).catch((reason) =>{ console.error('FELET ', reason)});
+    console.log('scrrein');
   await page.screenshot({path: 'aftonabladet.png'}).then(()=>{console.log('scriin');});
 
   await browser.close();
